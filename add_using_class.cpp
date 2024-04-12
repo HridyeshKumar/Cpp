a04a232c2465f11dbf288a55b864e13dd70975e2
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-04-12 11:28:15 +0530

    Add files via upload

---
+++ add_using_class.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+class Addition{
+    private:
+        int num1,num2,sum;
+    public:
+        void input(){
+            std::cout<<"Enter two number:";
+            std::cin>>num1>>num2;
+        }
+        void calculate(){
+            sum=num1+num2;
+        }
+        void output(){
+            std::cout<<"The sum of "<<num1<<" and "<<num2<<" is "<<sum<<std::endl;
+        }
+};
+int main(){
+    Addition add;
+    add.input();
+    add.calculate();
+    add.output();
+    return 0;
+}
