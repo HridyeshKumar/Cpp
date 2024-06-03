45628d434097d6e97b97030ec4cabc4911432c9c
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-03 14:57:31 +0530

    Create gcdUSINGgcdFunction.cpp

---
+++ gcdUSINGgcdFunction.cpp
@@ -0,0 +1,10 @@
+// CPP program to find gcd of two numbers using inbuilt __gcd() function 
+#include <algorithm> 
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+  int a,b;
+  cin>> a >>b;
+	cout << "gcd(a,b) = " << __gcd(a,b) << endl; 
+}
