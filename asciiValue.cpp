cc771d407a3c1d848d83430158b225f308446f22
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-03 13:27:37 +0530

    Create asciiValue.cpp

---
+++ asciiValue.cpp
@@ -0,0 +1,9 @@
+// C program to print ASCII Value of Character
+#include <iostream>
+using namespace std;
+int main()
+{
+	char c = 'k';
+	cout<<"The ASCII value of " << c << " is " << int (c);
+	return 0;
+}
