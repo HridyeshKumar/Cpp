c353160736901dcb51b5cd3e5cd759ae6f0041c3
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-20 07:54:51 +0530

    Create multipleInheritence.cpp

---
+++ multipleInheritence.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Parent1{
+public:
+  Parent(){
+    cout<<"Parent1 class"<<endl;
+  }
+};
+
+class Parent2{
+public:
+  Parent2(){
+    cout<<"Parent2 class"<<endl;
+  }
+};
+
+class Child:public Parent1,public Parent2{
+public:
+  Child(){
+    cout<<"Child class"<<endl;
+  }
+};
+
+int main(){
+  Child c;
+  return 0;
+}
