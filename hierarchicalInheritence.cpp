adc589087f0eeaa0ec4255de2c5c00945375071f
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-20 07:59:55 +0530

    Update hierarchicalInheritence.cpp

--- hierarchicalInheritence.cpp
+++ hierarchicalInheritence.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
 
+class Parent1{
+public:
+  Parent(){
+    cout<<"Parent1 class"<<endl;
+  }
+};
+
+class Parent2{
+public:
+  Parent2(){
+    cout<<"Parent2 class"<<endl;
+  }
+};
+
+class Child1:public Parent1{
+public:
+  Child1(){
+    cout<<"Child1 class"<<endl;
+  }
+};
+
+class Child2:public Parent1{
+public:
+  Child2(){
+    cout<<"Child2 class"<<endl;
+  }
+};
+
+int main(){
+  Child c;
+  return 0;
+}
