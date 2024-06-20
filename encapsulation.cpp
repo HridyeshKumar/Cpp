f38b3a39092a1a516ee51f9a04fa6fe0ad2e292c
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-20 07:24:51 +0530

    Create encapsulation.cpp

---
+++ encapsulation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class ABC{
+  int x;
+  public:
+  void set(int n){
+    x=n;
+  }
+  int get(){
+    return x;
+  }
+};
+
+int main(){
+  ABC obj1;
+  obj1.set(3);
+  cout<<obj1.get()<<endl;
+  return 0;
+}
