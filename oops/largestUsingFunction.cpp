6fb7e5c8d61520aebfd9571e40e079ea5ac2ed8a
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-07-15 21:42:28 +0530

    Create largestUsingFunction.cpp

---
+++ oops/largestUsingFunction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double n1, n2, n3;
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
+    if (n1 >= n2 && n1 >= n3)
+        cout << "Largest number: " << n1;
+    else if (n2 >= n1 && n2 >= n3)
+        cout << "Largest number: " << n2;
+    else
+        cout << "Largest number: " << n3;
+    return 0;
+}
