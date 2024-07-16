20608dcd2718b4e359c4d88513c5ccbfe71aa523
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-07-16 09:25:11 +0530

    Create exceptionHandling.cpp

---
+++ oops/exceptionHandling.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double numerator, denominator, divide;
+    cout << "Enter numerator: ";
+    cin >> numerator;
+    cout << "Enter denominator: ";
+    cin >> denominator;
+
+    try {
+        // Throw an exception if denominator is 0
+        if (denominator == 0)
+            throw 0; // You can throw any literal or variable here
+
+        // Not executed if denominator is 0
+        divide = numerator / denominator;
+        cout << numerator << " / " << denominator << " = " << divide << endl;
+    } catch (int num_exception) {
+        cout << "Error: Cannot divide by " << num_exception << endl;
+    }
+
+    return 0;
+}
