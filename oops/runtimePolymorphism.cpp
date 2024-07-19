65a4603597dc2668920f54fc98837dde9756ed82
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-07-19 10:21:16 +0530

    Create runtimePolymorphism.cpp

---
+++ oops/runtimePolymorphism.cpp
@@ -0,0 +1,13 @@
+class Base {
+public:
+    virtual void show() {
+        cout << "Base class" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void show() override {
+        cout << "Derived class" << endl;
+    }
+};
