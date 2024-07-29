526c336539d060e2065b86fa3094effdabd4d0b3
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-07-29 15:18:50 +0530

    Update compoundInterest.cpp

--- compoundInterest.cpp
+++ compoundInterest.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main()
 {
 	double principal = 10000, rate = 5, time = 2;
-	// Calculate compound interest
 	double A = principal * ((pow((1 + rate / 100), time)));
 	double CI = A - principal;
 	cout << "Compound interest is " << CI;
