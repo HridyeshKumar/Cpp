5b5d50ed0aa76c510d0af0b94b8068660bc31a75
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-09-23 02:16:10 +0530

    Update factorialOfNumber.cpp

--- factorialOfNumber.cpp
+++ factorialOfNumber.cpp
@@ -2,16 +2,14 @@
 #include <iostream>
 using namespace std;
 // Function to find factorial of given number
-unsigned int factorial(unsigned int n)
-{
-	if (n == 0)
-		return 1;
-	return n * factorial(n - 1);
+unsigned int factorial(unsigned int n){
+  if (n == 0)
+    return 1;
+  return n * factorial(n - 1);
 }
-int main()
-{
-	int num ;
+int main(){
+  int num ;
   cin>> n; 
-	cout << "Factorial of " << num << " is " << factorial(num) << endl;
-	return 0;
+  cout << "Factorial of " << num << " is " << factorial(num) << endl;
+  return 0;
 }
