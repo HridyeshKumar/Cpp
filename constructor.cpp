9ee75f1558aed91599d93fefb5df462682414daa
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-11-10 18:58:16 +0530

    Update constructor.cpp

--- constructor.cpp
+++ constructor.cpp
@@ -1,36 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Rectangle{
+class Rectangle
+{
 public:
-int l;
-int b;
+  int l;
+  int b;
 
-Rectangle(){ //default constructor - no args passed
-l=0;
-b=0;
-}
-Rectangle(int x,int y){ //parameterized constructor - args passed
-l=x;
-b=y;
-}
-Rectangle(Rectangle& r){ //copy constructor - initialise an obj by another existing obj
-l=r.l;
-b=r.b;
-}
-~Rectangle(){ //Destructor
-  cout<<"Destructor is called";
+  Rectangle()
+  { // default constructor - no args passed
+    l = 0;
+    b = 0;
+  }
+  Rectangle(int x, int y)
+  { // parameterized constructor - args passed
+    l = x;
+    b = y;
+  }
+  Rectangle(Rectangle &r)
+  { // copy constructor - initialise an obj by another existing obj
+    l = r.l;
+    b = r.b;
+  }
+  ~Rectangle()
+  { // Destructor
+    cout << "Destructor is called";
+  }
 };
 
-int main(){
-Rectangle* r0=new Rectangle();
-cout<<r0-><<" "<<r0->b<<endl;
-delete r0;
-Rectangle r1;
-cout<<r1.l<<" "<<r1.b<<endl;
-Rectangle r2(3,4);
-cout<<r2.l<<" "<<r2.b<<endl;
-Rectangle r3=r2;
-cout<<r3.l<<" "<<r3.b<<endl;
+int main()
+{
+  Rectangle *r0 = new Rectangle();
+  cout << r0->l << " " << r0->b << endl;
+  delete r0;
+  Rectangle r1;
+  cout << r1.l << " " << r1.b << endl;
+  Rectangle r2(3, 4);
+  cout << r2.l << " " << r2.b << endl;
+  Rectangle r3 = r2;
+  cout << r3.l << " " << r3.b << endl;
 
-return 0;
+  return 0;
 }
